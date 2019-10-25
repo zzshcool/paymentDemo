6aa7bc02e920394d051ef67c4fb71bbdf6b03e98
Jasoncc.Chen <Jasoncc.Chen@csnt.com.tw>
2019-10-25 14:51:00 +0800

    swagger & security

---
+++ src/main/java/com/example/paymentDemo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.paymentDemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("root").password("{noop}root").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}123456").roles("ADMIN");
+    }
+
+}
---
+++ src/main/java/com/example/paymentDemo/controller/PaymentService.java
@@ -0,0 +1,6 @@
+package com.example.paymentDemo.controller;
+
+
+public interface PaymentService {
+
+}
